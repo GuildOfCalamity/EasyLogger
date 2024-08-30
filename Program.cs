a8ee979c8b606e59a72cb49aeb73c44bd1fb55ad
GuildOfCalamity <81915293+GuildOfCalamity@users.noreply.github.com>
2024-08-30 16:54:45 -0400

    added entry point

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logger;
+
+namespace EasyLogger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine($"{Environment.NewLine}• Testing Plain {nameof(LoggerBase)}…");
+            using (LoggerBase log = new DeferredLogger(Path.Combine(Directory.GetCurrentDirectory(), $"LoggerPlain.txt")))
+            {
+                log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test started.");
+                /** something extra could go here **/
+                log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test finished.");
+
+                Console.WriteLine($"{log.LogFilePath()}");
+            }
+
+            Console.WriteLine($"{Environment.NewLine}• Testing Buffered {nameof(LoggerBase)}…");
+            using (LoggerBase log = new BufferedLogger(Path.Combine(Directory.GetCurrentDirectory(), $"LoggerBuffered.txt")))
+            {
+                log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test started.");
+                /** something extra could go here **/
+                log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test finished.");
+
+                Console.WriteLine($"{log.LogFilePath()}");
+            }
+
+            Console.WriteLine($"{Environment.NewLine}• Testing Queued {nameof(LoggerBase)}…");
+            using (LoggerBase log = new QueuedLogger(Path.Combine(Directory.GetCurrentDirectory(), $"LoggerQueued.txt")))
+            {
+                log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test started.");
+                /** something extra could go here **/
+                log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test finished.");
+
+                Console.WriteLine($"{log.LogFilePath()}");
+            }
+
+            Console.Write($"{Environment.NewLine}• Press any key to exit…");
+            _ = Console.ReadKey(true);
+        }
+    }
+}
