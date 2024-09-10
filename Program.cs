52db83723e5c758ea6ffa90f7d1afecf82826eb1
GuildOfCalamity <81915293+GuildOfCalamity@users.noreply.github.com>
2024-09-10 16:52:01 -0400

    moved tests

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Text;
 using Logger;
 
@@ -13,9 +16,10 @@ public class Program
         Console.OutputEncoding = Encoding.UTF8;
         AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
         Console.CancelKeyPress += new ConsoleCancelEventHandler(ConsoleHandler);
+        ListReferencedAssemblies();
         #endregion
 
-        TestLogger.Run();
+        TestLoggers.Run();
 
         PressKeyTo("EXIT", ConsoleKey.Escape);
     }
@@ -51,5 +55,166 @@ public class Program
         Console.CursorVisible = true;
         Environment.Exit(1);
     }
+
+    static void ListReferencedAssemblies()
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        AssemblyName main = assembly.GetName();
+        Console.WriteLine($"Main Assembly: {main.Name}, Version: {main.Version}");
+        foreach (var sas in assembly.GetReferencedAssemblies().OrderBy(o => o.Name))
+        {
+            Console.WriteLine($" Sub Assembly: {sas.Name}, Version: {sas.Version}");
+        }
+    }
     #endregion
+
 }
+
+#region [Testing]
+/// <summary>
+/// Basic test class.
+/// </summary>
+public static class TestLoggers
+{
+    public static void Run()
+    {
+        #region [BufferedLogger]
+        Console.WriteLine($"{Environment.NewLine}• Testing Buffered {nameof(LoggerBase)}…");
+        using (LoggerBase log = new BufferedLogger(Path.Combine(Directory.GetCurrentDirectory(), $"LoggerBuffered.txt")))
+        {
+            log.TimeFormat = "MM-dd-yyyy hh:mm:ss.fff tt";
+            log.OnException += (ex) => { Debug.WriteLine($"[WARNING] {ex.Message}"); };
+
+            log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test started.");
+            /** 
+                something extra could go here 
+            **/
+            log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test finished.");
+
+            Console.WriteLine($"{log.LogFilePath}");
+        }
+        #endregion
+
+        #region [QueuedLogger]
+        Console.WriteLine($"{Environment.NewLine}• Testing Queued {nameof(LoggerBase)}…");
+        using (LoggerBase log = new QueuedLogger(Path.Combine(Directory.GetCurrentDirectory(), $"LoggerQueued.txt")))
+        {
+            log.TimeFormat = "MM/dd/yyyy hh:mm:ss.fff tt";
+            log.OnException += (ex) => { Debug.WriteLine($"[WARNING] {ex.Message}"); };
+
+            log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test started.");
+            /** 
+                something extra could go here 
+            **/
+            log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test finished.");
+
+            Console.WriteLine($"{log.LogFilePath}");
+        }
+        #endregion
+
+        #region [TokenLogger]
+        Console.WriteLine($"{Environment.NewLine}• Testing Token {nameof(LoggerBase)}…");
+        using (LoggerBase log = new TokenLogger("")) // Let the logger decide on the file name.
+        {
+            log.OnException += (ex) => { Debug.WriteLine($"[WARNING] {ex.Message}"); };
+
+            log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test started.");
+            /** 
+                something extra could go here 
+            **/
+            log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test finished.");
+
+            Console.WriteLine($"{log.LogFilePath}");
+        }
+        #endregion
+
+        #region [WaitHandleLogger]
+        // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+        Console.WriteLine($"{Environment.NewLine}• Testing WaitHandle {nameof(LoggerBase)}…");
+        using (LoggerBase log = new HandleLogger(Path.Combine(Directory.GetCurrentDirectory(), $"LoggerHandle.txt")))
+        {
+            log.OnException += (ex) => { Debug.WriteLine($"[WARNING] {ex.Message}"); };
+
+            log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test started.");
+
+            for (int i = 1; i < 21; i++)
+            {
+                log.Write($"Index #{i}");
+            }
+
+            log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test finished.");
+
+            Console.WriteLine($"{log.LogFilePath}");
+        }
+        #endregion
+
+        #region [DeferredLogger]
+        Console.WriteLine($"{Environment.NewLine}• Testing Deferred {nameof(LoggerBase)}…");
+        using (LoggerBase log = new DeferredLogger(Path.Combine(Directory.GetCurrentDirectory(), $"LoggerDeferred.txt")))
+        {
+            log.TimeFormat = "yyyy-MM-dd hh:mm:ss.fff tt";
+            log.OnException += (ex) => { Debug.WriteLine($"[WARNING] {ex.Message}"); };
+
+            log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test started.");
+            for (int i = 1; i < 6; i++) 
+            { 
+                Thread.Sleep(10); 
+                log.Write($"Index #{i}"); 
+            }
+
+            log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test finished.");
+
+            Console.WriteLine($"{log.LogFilePath}");
+        }
+        #endregion
+
+        #region [HashSetLogger]
+        Console.WriteLine($"{Environment.NewLine}• Testing HashSet {nameof(LoggerBase)}…");
+        using (LoggerBase log = new HashSetLogger(Path.Combine(Directory.GetCurrentDirectory(), $"LoggerHashSet.txt")))
+        {
+            ((HashSetLogger)log).DaysUntilWipe = 0.5; // Example of changing the flush trigger.
+            log.OnException += (ex) => { Debug.WriteLine($"[WARNING] {ex.Message}"); };
+            Task.Run(() =>
+            {
+                for (int i = 1; i < 101; i++)
+                {
+                    log.Write($"This should only appear once in the log file.");
+                }
+            });
+            log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test started.");
+            for (int i = 1; i < 101; i++)
+            {
+                log.Write($"This should only appear once in the log file.");
+            }
+            log.Write($"{log.GetType()?.Name} of base type {log.GetType()?.BaseType?.Name} - Test finished.");
+            Console.WriteLine($"{log.LogFilePath}");
+        }
+        #endregion
+
+        #region [IntervalLogger]
+        // This test is different since we want to keep the object around for some time to test interval writing.
+        Console.WriteLine($"{Environment.NewLine}• Testing Interval {nameof(LoggerBase)}…");
+        LoggerBase iLog = new IntervalLogger(Path.Combine(Directory.GetCurrentDirectory(), $"LoggerInterval.txt"), TimeSpan.FromSeconds(10));
+        iLog.OnException += (ex) => { Debug.WriteLine($"[WARNING] {ex.Message}"); };
+        iLog.LogFileEncoding = Encoding.ASCII; // Example of changing file encoding after object creation.
+        ((IntervalLogger)iLog).WriteInterval = TimeSpan.FromSeconds(2); // Example of changing time interval after object creation.
+        iLog.Write($"{iLog.GetType()?.Name} of base type {iLog.GetType()?.BaseType?.Name} - Test started.");
+        for (int i = 1; i < 6; i++)
+        {
+            Thread.Sleep(10);
+            // There is no need to call IsAllowed() from the user's point of view, for the test it serves as an awaiter.
+            while (!((IntervalLogger)iLog).IsAllowed())
+            {
+                Console.Write($"•");
+                Thread.Sleep(333);
+            }
+            iLog.Write($"Index #{i}");
+        }
+        Console.WriteLine();
+        iLog.Write($"{iLog.GetType()?.Name} of base type {iLog.GetType()?.BaseType?.Name} - Test finished.");
+        Console.WriteLine($"{iLog.LogFilePath}");
+        iLog.Dispose();
+        #endregion
+    }
+}
+#endregion
--- README.md
+++ README.md
@@ -9,11 +9,11 @@
 | NET Framework | 4.8.1 |
 
 - A logger library which offers deferred writing so as to not block the main execution thread.
-- There are six flavors offered:
+- There are seven flavors offered:
 	- **Deferred Logger**
     	- A thread created for each write request via SemaphoreSlimEx.
 	- **Buffered Logger**
-    	- Threads created for each write that access a ConcurrentQueue with a Timer.
+    	- Threshold-based where tasks are created that access a ConcurrentQueue with a Timer.
 	- **Queued Logger**
     	- A background, low priority thread monitors a BlockingCollection.
 	- **Token Logger**
@@ -22,7 +22,10 @@
     	- A traditional EventWaitHandle setup with one background thread accessing a Queue.
 	- **Interval Logger**
     	- A background thread monitors a BlockingCollection with last-write interval checking.
+	- **HashSet Logger**
+    	- A background thread monitors a HashSet for cutting down on duplicate entries.
 
+- The final library is small, only **26 KB**. You could make it smaller by building with only the logger you prefer.
 - This solution includes a console application for testing the logger DLL.
 - I've added a custom [SemaphoreSlim](https://learn.microsoft.com/en-us/dotnet/api/system.threading.semaphoreslim?view=netframework-4.8.1) for determining when it has been disposed. Ironically the BCL **SemaphoreSlim** does contain a **CheckDisposed()** method, but it's private.
 	- From the [Dispose](https://learn.microsoft.com/en-us/dotnet/api/system.threading.semaphoreslim.dispose?view=netframework-4.8.1) notes: "*Always call Dispose before you release your last reference to the SemaphoreSlim. Otherwise, the resources it is using will not be freed until the garbage collector calls the SemaphoreSlim object's Finalize method.*"
